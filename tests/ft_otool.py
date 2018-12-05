7dc9ecdca348bfd24278bca3e34369865fbc76e7
Leo SIMON <lsimon@e1r7p6.42.fr>
2018-12-05 09:46:12 +0100

    64: tests added

--- tests/ft_otool.py
+++ tests/ft_otool.py
@@ -50,9 +50,16 @@ class M32(Base):
 		self.test_path = os.path.join(dir_path, "custom_tests/32")
 		self.files = os.listdir(self.test_path)
 
-	def test_easy(self):
+	def test_32(self):
 		self.compare(self.files)
 
+class M64(Base):
+	def setUp(self):
+		self.test_path = os.path.join(dir_path, "custom_tests/64")
+		self.files = os.listdir(self.test_path)
+
+	def test_64(self):
+		self.compare(self.files)
 
 # class Fat(Base):
 # 	def setUp(self):
